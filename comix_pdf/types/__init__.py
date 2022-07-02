54cf58cb091555e92399d872f94622e75326fd04
Rud356 <rud356github@gmail.com>
2022-07-02 03:20:21 +0300

    Export custom types

---
+++ comix_pdf/types/__init__.py
@@ -0,0 +1,9 @@
+"""
+Types for manipulating content of outputted pdf file and collected images.
+All work related to rendering images to pdf and needed for that types are here.
+"""
+
+from .comics import Comics
+from .excluded_images import ExcludedImages, ExcludedImage
+from .fill_color import FillColor
+from .image import ComicsImage
